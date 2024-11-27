7b18ad4a4f8da76cd64eaeead94a510ae3317123
Matvey Romodanovskiy <startpolus@gmail.com>
2024-11-27 16:29:34 +0200

    add array with result

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 const calculator = (a,b,operationCallback)=>{
     return operationCallback(a,b);
 }
+const results = [];
 const operations = {
     "+" : (a,b) => a+b,
     "-" :(a,b) => a-b,
@@ -35,16 +36,42 @@ const getOperation = () =>{
 
 //Main function
 const runCalculator = () => {
+    let result;
     getNumber.status = undefined;
     const num1 = getNumber("Enter the first number");
     const num2 = getNumber("Enter the second number");
     const operation = getNumber.status === null? null : getOperation();
-    const result = operation !== null? calculator(num1, num2, operations[operation]) :" The operation was cancelled.";
+    if (operation){
+         result = calculator(num1, num2, operations[operation]);
+         if(isUnique(results, num1, num2, operations[operation])){
+             results.push({
+                 a: num1,
+                 b: num2,
+                 operation: operations[operation],
+                 result: result
+             });
+         }
+         else {
+                 alert("This calculation already exists in the history.");
+             }
+         } else {
+        result = " The operation was cancelled.";
+    }
     alert(`The result is: ${result}`);
     if(confirm("Do you want to perform another operation?")){
         runCalculator();//Recursively start again
     }else{
         alert(`Thank you for using calculator`);
+        console.log("Calculation history:", results);
     }
 };
+function isUnique(results, num1, num2, operation) {
+    for (let i = 0; i < results.length; i++) {
+        const item = results[i];
+        if (item.a === num1 && item.b === num2 && item.operation === operation) {
+            return false; // not unique
+        }
+    }
+    return true; // unique
+}
 runCalculator();
